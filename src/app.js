813afeb469df08ae8447b8f3657db1161816edd7
Ayush Jaiswal <ayush42837272@gmail.com>
2026-03-31 12:43:08 +0530

    database sanitation done

--- src/app.js
+++ src/app.js
@@ -3,26 +3,65 @@ const { connectDB } = require("./config/database");
 const { User } = require("./models/user");
 const app = express();
 
-app.post("/signup", async(req, res) => {
-  const user = new User({
-    firstName:"Nikita",
-    lastName:"Jaiswal",
-    emailId:"test2@gmail.com",
-    password:"test1234",
-    age:22,
-    gender:"female" 
+app.use(express.json());
 
-  })
+app.post("/signup", async(req, res) => {
+  const userData = req.body;
+  const user = new User(userData);
   try{
   await user.save()
-  res.send("User added successfully");
+  res.send("User added successfully mittar");
   }
   catch(err){
-    res.status(500).send("Error adding user");
+    res.status(500).send(err.message);
   }
 
 });
 
+app.post("/fetchAllUsers", async(req, res) => {
+  try{
+    const users = await User.find();
+    res.json(users);
+  }
+  catch(err){
+    console.error("Error fetching users", err.message);
+    res.status(500).send(err.message);
+  }
+})
+
+app.delete("/user/:id", async(req, res) => {
+  const userId = req.params.id;
+  try{
+    await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  }
+  catch(err){
+    console.error("Error deleting user", err.message);
+    res.status(500).send(err.message);
+  } 
+});
+
+app.patch("/user/:id", async(req, res) => {
+  const userId = req.params.id;
+  const updateData = req.body;
+  const ALLOWED_UPDATES = ["firstName", "lastName", "skills", "shortDesc"];
+  const isValidUpdate = Object.keys(updateData).every((key) => ALLOWED_UPDATES.includes(key));
+
+  if(updateData.skills.length>10){
+    return res.status(400).send("Skills cannot have more than 10 items.");
+  }
+  if(!isValidUpdate){
+    return res.status(400).send("Invalid updates! Only firstName, lastName, skills and shortDesc can be updated.");
+  }
+  try{
+    await User.findByIdAndUpdate(userId, updateData, {new: true, runValidators: true});
+    res.send("User updated successfully");
+  }
+  catch(err){
+    console.error("Error updating user", err.message);
+    res.status(500).send(err.message);
+  } 
+});
 connectDB()
   .then(() => {
     console.log("Connected to MongoDB");
--- src/models/user.js
+++ src/models/user.js
@@ -3,23 +3,41 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     firstName:{
         type:String,
+        required:true,
+        minlength: 3,
     },
     lastName:{
         type:String,
     },
     emailId:{
         type:String,
+        required:true,
+        trim: true,
     },
     password:{
         type:String,
     },
     age:{
         type:Number,
+        min: 18,
+        
     },
     gender:{
         type:String,
+        validate(value){
+            if(!(["male", "female", "other"].includes(value.toLowerCase()))){
+                throw new Error("Gender must be either  male, female or other")
+            }
+        }
     },
-})
+    skills:{
+        type:[String],
+    },
+    shortDesc:{
+        type:String,
+        default: "This is short description about me",
+    }
+}, {timestamps: true})
 
 const User = mongoose.model("users", userSchema)
 
