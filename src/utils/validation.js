9f48cbe8a35383962644a0a0f9c91c3d63ef9eb7
Ayush Jaiswal <ayush42837272@gmail.com>
2026-04-04 09:36:53 +0530

    Add validation functions for update and password reset

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -14,4 +14,25 @@ const validateSignupData = (data) => {
     return {isValid: true};
 }
 
-module.exports = {validateSignupData}
+
+const validateUpdateData = (data) => {
+    const allowedEditFields = ["firstName", "lastName", "skills", "shortDesc"];
+    const isValidUpdate = Object.keys(data).every((key) => allowedEditFields.includes(key));
+    if(data.skills && data.skills.length > 10){
+        return {isValid: false, message: "Skills cannot have more than 10 items."};
+    }   
+    if (!isValidUpdate) {
+        return {isValid: false, message: "Invalid updates! Only firstName, lastName, skills and shortDesc can be updated."};
+    }
+    return {isValid: true};
+}
+
+const validatePasswordResetData = (data) => {
+    const {oldPassword, newPassword} = data;
+    if(!validator.isStrongPassword(newPassword, {minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1})){
+        return {isValid: false, message: "New password must be at least 8 characters long and include at least one lowercase letter, one uppercase letter, one number, and one symbol."};
+    }
+    return {isValid: true};
+}
+
+module.exports = {validateSignupData, validateUpdateData, validatePasswordResetData}
